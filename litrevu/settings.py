496a1dc6effb56a92dd9812790bd9a18fa01960a
Christian Debray <christian.debray5@gmail.com>
2024-10-07 10:48:24 +0200

    use debug_toolbar when running app locally in debug mode

--- litrevu/settings.py
+++ litrevu/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'bootstrap5'
+    'bootstrap5',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'litrevu.urls'
@@ -163,3 +165,8 @@ LOGGING = {
 }
 
 LOGIN_URL = "/litrevu/auth"
+
+# define INTERNAL_IP for the debug_toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
