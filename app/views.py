96983c61c5ab47588a61a98870aec0461126659c
Christian Debray <christian.debray5@gmail.com>
2024-10-09 10:12:29 +0200

    fix _add_next_url helper when no default value is available

--- app/views.py
+++ app/views.py
@@ -277,15 +277,24 @@ def _get_next_route(request: HttpRequest, default: str) -> str:
     return request.POST.get("next") or request.GET.get("next") or default
 
 
-def _add_next_url(url: str, request: HttpRequest, next_url: str = None, default: str = None) -> str:
-    """Appends or updates the 'next" query parameter in a URL."""
-    next_url = next_url or _get_next_route(request, default)
+def _add_next_url(url: str, request: HttpRequest, next_url: str = None) -> str:
+    """Appends or updates the 'next" query parameter in a URL.
+    Helps customizing redirects after processing a form, for instance.
+
+    The next_url string can be either a view name as defined in the URLConf or a full URL.
+
+    If the second parameter is not set, tries to find the next url in the current request.
+    If no 'next' query parameter is found nor set, returns the url without changes.
+    """
+    next_url = next_url or _get_next_route(request, next_url)
     if next_url:
         parts = parse.urlsplit(url)
         qs = parse.parse_qs(parts.query) or {}
         qs.update({'next': next_url})
         new_query = parse.urlencode(qs)
         return parse.urlunsplit([parts.scheme, parts.netloc, parts.path, new_query, parts.fragment])
+    else:
+        return url
 
 
 @login_required
