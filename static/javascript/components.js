337b6e07be4f031028f442752724487884438bf2
Christian Debray <christian.debray5@gmail.com>
2024-10-16 15:11:38 +0200

    refactor custom elements

--- static/javascript/components.js
+++ static/javascript/components.js
@@ -1,53 +1,49 @@
 /**
  * A custom element displaying a star symbol as an SVG graphic.
+ * SVG graphics are loaded from a template identified by id="star-template".
+ * The template must have been loaded in the DOM before instanciating the widget.
  */
-class StarWidget extends HTMLElement {
+class StarSymbol extends HTMLElement {
     constructor() {
         super();
-        this.template = document.createElement('template');
-        this.template.innerHTML = this.templateHTML;
     }
 
-    templateHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 34.4" aria-hidden="true">
-        <path d="M28.39,33.12,17.67,27,6.93,33.12,10,20.94l-7.77-8.1H12.85L17.67,2.55l4.83,10.28H33.1l-7.78,8.1Z"
-            transform="translate(-0.18 -0.47)"></path>
-    </svg>
-    `;
+    static stylesheetURL = ''
 
     connectedCallback() {
         const shadow = this.attachShadow({mode: "open"});
-        const tpl = document.createElement('template');
-        tpl.innerHTML = this.templateHTML;
+        const tpl = document.getElementById('star-symbol-template')
         shadow.appendChild(tpl.content.cloneNode(true))
-        // Apply external styles to the shadow dom
-        const linkElem = document.createElement("link");
-        linkElem.setAttribute("rel", "stylesheet");
-        linkElem.setAttribute("href", "/static/css/components.css");
-        // Attach the created elements to the shadow dom
-        shadow.appendChild(linkElem);
+        // if available, apply external styles to the shadow dom
+        if (StarSymbol.stylesheetURL) {
+            const linkElem = document.createElement("link");
+            linkElem.setAttribute("rel", "stylesheet");
+            linkElem.setAttribute("href", StarSymbol.stylesheetURL);
+            shadow.appendChild(linkElem);
+        }
     }
 }
-
-customElements.define("star-widget", StarWidget);
 
 /**
  * A widget displaying a rating as a sequence of stars.
  * 
  * Stars can be full or empty : for ex, a score of 3/5 will be displayed as 3 full stars followed by 2 empty stars.
  *
+ * The rating widget uses the StarSymbol custom element to render stars.
+ * 
  * css accessors:
  * - full stars are accessed by the .full-star class
  * - empty stars are accessed by the .empty-star class
  * - see the properties definded in components.css to customize the look of the widget.
  */
 class RatingWidget extends HTMLElement {
-    defaults = {
-        default_alt_text: "rating",
-        default_star_symbol: "*",
-        default_coalesce_undefined: "(no rating)",
+    static defaults = {
+        alt_text: "rating",
+        coalesce_undefined: "(no rating)",
         max_rating: 5
     }
+
+    static stylesheetURL = ''
 
     constructor() {
         super();
@@ -56,22 +52,27 @@ class RatingWidget extends HTMLElement {
     connectedCallback() {
         const shadow = this.attachShadow({mode: "open"});
         const rating = this.getAttribute("data-rating");
-        const maxRating = this.getAttribute("data-max-rating") || this.defaults.max_rating;
-        const ratingTxt = rating != null ? rating + "/" + maxRating: this.defaults.default_coalesce_undefined;
-        const labelTxt = this.getAttribute("aria-label") || this.defaults.default_alt_text + ": " + ratingTxt;
+        const maxRating = this.getAttribute("data-max-rating") || RatingWidget.defaults.max_rating;
+        const ratingTxt = rating != null ? rating + "/" + maxRating: RatingWidget.defaults.coalesce_undefined;
+        const labelTxt = this.getAttribute("aria-label") || RatingWidget.defaults.alt_text + ": " + ratingTxt;
         
         const wrapper = document.createElement("span");
         wrapper.setAttribute("class", "wrapper");
-        wrapper.setAttribute("aria-valuenow", rating);
-        wrapper.setAttribute("aria-valuemin", 0);
-        wrapper.setAttribute("aria-valuemax", maxRating);
+        wrapper.setAttribute("role", "image");
         wrapper.setAttribute("aria-label", labelTxt);
         wrapper.setAttribute("title", labelTxt)
         shadow.appendChild(wrapper);
 
+        if (RatingWidget.stylesheetURL) {
+            const linkElem = document.createElement("link");
+            linkElem.setAttribute("rel", "stylesheet");
+            linkElem.setAttribute("href", RatingWidget.stylesheetURL);
+            shadow.appendChild(linkElem);
+        }
+
         if (rating != null) {
             for(let i = 1; i <= maxRating; i++) {
-                const starEl = document.createElement("star-widget");
+                const starEl = document.createElement("star-symbol");
                 let starElClasses = "star";
                 if (i <= rating) {
                     starElClasses += " star-full";
@@ -87,12 +88,35 @@ class RatingWidget extends HTMLElement {
             noRatingText.textContent = altText;
             wrapper.appendChild(noRatingText);
         }
-
-        const linkElem = document.createElement("link");
-        linkElem.setAttribute("rel", "stylesheet");
-        linkElem.setAttribute("href", "/static/css/components.css");
-        shadow.appendChild(linkElem);
     }
  }
 
-customElements.define("rating-widget", RatingWidget);
+
+async function loadTemplates (filename) {
+    const res = await fetch(filename)
+    const text = await res.text()
+  
+    document.body.insertAdjacentHTML('beforeend', text)
+    return document.body.lastElementChild
+}
+
+function initComponents() {
+    const loader = document.querySelector('script#component-loader');
+    const templatesURL = loader.dataset.templatesUrl;
+    const componentStylesheet = loader.dataset.stylesheetUrl;
+    if (componentStylesheet) {
+        StarSymbol.stylesheetURL = componentStylesheet;
+        RatingWidget.stylesheetURL = componentStylesheet;
+    }
+    if (templatesURL) {
+        Promise.all([loadTemplates(templatesURL)]).then(() => {
+            customElements.define("star-symbol", StarSymbol);
+            customElements.define("rating-widget", RatingWidget);
+        });
+    } else {
+        customElements.define("star-symbol", StarSymbol);
+        customElements.define("rating-widget", RatingWidget);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', initComponents);
